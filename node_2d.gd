5ceba5133ec19eba202436249fd274e463d126bc
KaiserinJacky <jacky.gaffney@gmail.com>
2024-06-01 16:19:26 -0400

    Added Script to scene

---
+++ node_2d.gd
@@ -0,0 +1,12 @@
+# this area is for scripting code for the scene; each node can have its own set of scripts.
+
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
