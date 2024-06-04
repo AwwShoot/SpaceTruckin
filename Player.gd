ff1261437889726f8db4f2f8f9ab8db1b9fd9a15
KaiserinJacky <jacky.gaffney@gmail.com>
2024-06-04 14:07:48 -0400

    added motion to character and added animations

--- Player.gd
+++ Player.gd
@@ -1,4 +1,15 @@
 extends CharacterBody2D
 
-func _physics_process(_delta): #remove the _ before delta when fun
-	pass
+var speed = 100
+
+func _process(delta):
+	var direction = 0
+	if Input.is_action_pressed("ui_left"):
+		position.x -= speed * delta
+	if Input.is_action_pressed("ui_right"):
+		position.x += speed * delta
+	if Input.is_action_pressed("ui_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("ui_down"):
+		position.y += speed * delta
+	
