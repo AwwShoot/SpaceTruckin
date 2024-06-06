4c38f1b7312851105619b87327eff686a0e38aa8
KaiserinJacky <jacky.gaffney@gmail.com>
2024-06-06 15:48:31 -0400

    messed with some stuff, dw about it
    man the thing about these names getting less helpful as time goes on is TRUE

--- Player.gd
+++ Player.gd
@@ -1,15 +1,19 @@
 extends CharacterBody2D
 
 var speed = 100
+var direction = 0
 
 func _process(delta):
-	var direction = 0
 	if Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta
+		direction = 1
 	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta
+		direction = 3
 	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
+		direction = 2
 	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
+		direction = 0
 	
