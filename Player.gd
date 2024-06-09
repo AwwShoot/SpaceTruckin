1494126872f6ec9ff9156515a583bf722b9564f3
KaiserinJacky <jacky.gaffney@gmail.com>
2024-06-09 14:58:42 -0400

    changed window sizing and worked more on player movement and animation

--- Player.gd
+++ Player.gd
@@ -3,8 +3,16 @@ extends CharacterBody2D
 # constants && variables
 const speed = 100
 
-var direction = 0
-var moving = false
+var direction = 0 # 0-3, each corresponding with a direction
+var moving = false # determines if animation plays
+
+func placeguy(): # feel free to give this a better name, anyone
+	# initialize direction & idle pose
+	$PlayerSpriteAnimated.animation = "walk_down"
+	$PlayerSpriteAnimated.frame = 3
+	
+func _ready():
+	placeguy()
 
 func _process(delta):
 	moving = false # set moving to false so, if you're not moving, the animation doesn't play
@@ -14,6 +22,9 @@ func _process(delta):
 		direction = 2
 		moving = true
 		$PlayerSpriteAnimated.animation = "walk_up"
+		# if player is inputting multiple directions at time,
+		# no animation will play because it constantly circles between the two
+		# and doesn't get a chance to advance frames
 	if Input.is_action_pressed("move_down"):
 		position.y += speed * delta
 		direction = 0
@@ -32,3 +43,6 @@ func _process(delta):
 	# animate
 	if moving:
 		$PlayerSpriteAnimated.play()
+	if not moving:
+		$PlayerSpriteAnimated.stop()
+		$PlayerSpriteAnimated.frame = 3 # set frame to an idle standing
